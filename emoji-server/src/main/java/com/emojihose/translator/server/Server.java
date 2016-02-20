1eb3446d9a6aba0f0fe43ab31239f8b5b7609e68
Philip Youssef <philip.youssef@lendup.com>
2016-02-20 15:17:05 -0800

    added frontend

--- emoji-server/src/main/java/com/emojihose/translator/server/Server.java
+++ emoji-server/src/main/java/com/emojihose/translator/server/Server.java
@@ -1,20 +1,43 @@
 package com.emojihose.translator.server;
 
 import com.emojihose.translator.server.controller.TranslatorController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ro.pippo.controller.ControllerApplication;
 import ro.pippo.core.Pippo;
+import ro.pippo.core.RedirectHandler;
+import ro.pippo.core.TemplateHandler;
+import ro.pippo.core.route.RouteContext;
+import ro.pippo.core.route.RouteHandler;
 
 public class Server {
 
+    private static final Logger log = LoggerFactory.getLogger(Server.class);
+    
     public static void main(String[] args) {
         Pippo pippo = new Pippo(new EmojiTranslatorApp());
+        pippo.getApplication().addPublicResourceRoute();
         pippo.start();
     }
 
     static class EmojiTranslatorApp extends ControllerApplication {
         @Override
         protected void onInit() {
-            GET("/translate", TranslatorController.class, "translate");
+            
+            // Audit filter
+            ALL("/.*", new RouteHandler() {
+                @Override
+                public void handle(RouteContext routeContext) {
+                    log.info("Request for {} '{}'", routeContext.getRequestMethod(), routeContext.getRequestUri());
+                    routeContext.next();
+                }
+            });
+
+            // Web resources
+            GET("/", new TemplateHandler("base"));
+            
+            // API
+            GET("/api/translate", TranslatorController.class, "translate");
         }
     }
 }
---
+++ emoji-server/src/main/resources/templates/base.ftl
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html ng-app="crudNgApp">
+    <head>
+        <meta charset="utf-8">
+        <meta content="IE=edge" http-equiv="X-UA-Compatible">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Emoji Translator</title>
+
+        <link href="${publicAt('css/style.css')}" rel="stylesheet">
+        <base href="${contextPath}/">
+    </head>
+    <body>
+    HELLO WORLDS!
+        <div class="container">
+            
+            <script src="${publicAt('js/foo.js')}"></script>
+        </div>
+    </body>
+</html>
